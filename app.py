64a1c2b9fe052be02d1d98b88da6cf23a86f4be5
proofGu <g.a.a.guskov@gmail.com>
2021-02-16 17:58:37 +0300

    add css

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
+# render_template функция для работы с HTML
+# url_for фкнкция шаблонизатора
 app = Flask(__name__)  # передаем конструктором обекта класса __name__ имя этого файла
 
 
 @app.route('/')  # с помощью декоратора и функции (route) указываем путь который хотим отслеживать
 # ('/' - главная страница)
+@app.route('/home')
 def index():  # создаем свою функцию для отслеживания страницы
-    return "Hello World"  # просто будем выводить текст
+    return render_template('index.html')  # выводим файл с html шаблоном, по умолчанию в папке teamplates
 
 
 @app.route('/about')
 def about():
-    return "About page"
+    return render_template('about.html')
+
+
+@app.route('/user/<string:name>/<int:id>')  # получаем данные их адреса
+def user(name, id):
+    return 'User page: ' + name + ' - ' + str(id)
 
 
 if __name__ == '__main__':  # если запускаем через этот файл(app.py) = то запускаем проект как flask приложение
